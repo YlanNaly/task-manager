07d1ed30f6f21777beebfc3fdc810bc4c235f18a
Ylan <hei.nalisoa@gmail.com>
2023-06-15 11:18:11 +0300

    feat: use localstorage

--- src/hooks/useLocalStorage.ts
+++ src/hooks/useLocalStorage.ts
@@ -1,5 +1,11 @@
-const useLocalStorage = () => {
-
+const useLocalStorage = (key:any , value :any) => {
+  const storeItem = localStorage.getItem(key);
+  let parsedValue = storeItem ? JSON.parse(storeItem) : value;
+  
+  const setValue = (newValue:any) => {
+    parsedValue = newValue;
+    localStorage.setItem(key, JSON.stringify(newValue));
+  };
 }
 
 export {
