6e67c58ebff7be5af07ce30ce3ef7a32df26d5ad
Ylan <hei.nalisoa@gmail.com>
2023-06-15 10:02:38 +0300

    feat: time diff partie 2-1 exam

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,20 +1,43 @@
+import { GetServerSideProps } from "next";
 import Head from "next/head";
 import { useRouter } from "next/router";
-
 /**
   Calculates the time difference between the server time and client time.
   @param {Date} serverTime - The server time.
   @param {Date} clientTime - The client time.
   @returns {string} The time difference in the format "{days} days, {hours} hours, {minutes} minutes, {seconds} seconds".
 */
-const calculateTimeDifference = (server: Date, client: Date) => {};
+const calculateTimeDifference = (server: Date, client: Date) => {
+  console.log(server)
+  return client.getTime() - new Date(server).getTime()
+}
+const formatDateTime = (date: Date): string => {
+  const options: Intl.DateTimeFormatOptions = {
+    day: '2-digit',
+    hour: '2-digit',
+    minute: '2-digit',
+    second: "2-digit"
+  };
+  return date.toLocaleDateString('en-GB', options);
+};
+
+export async function getServerSideProps() {
+  const creationDate = formatDateTime(new Date());
+  return {
+    props: {
+      creationDate,
+    },
+  };
+}
 
 
-export default function Home() {
+export default function Home({creationDate}:any) {
   const router = useRouter();
   const moveToTaskManager = () => {
     router.push("/tasks");
   }
+  const csDate = new Date(); 
+  const diff = calculateTimeDifference(creationDate,csDate);
   return (
     <>
       <Head>
@@ -29,13 +52,13 @@ export default function Home() {
           {/* Display here the server time (DD-MM-AAAA HH:mm)*/}
           <p>
             Server time:{" "}
-            <span className="serverTime">{/* Replace with the value */}</span>
+            <span className="serverTime">{creationDate}</span>
           </p>
 
           {/* Display here the time difference between the server side and the client side */}
           <p>
             Time diff:{" "}
-            <span className="serverTime">{/* Replace with the value */}</span>
+            <span className="serverTime">{diff}</span>
           </p>
         </div>
 
